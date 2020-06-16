40dd4ee5d9b51d86f6d344ebb8872e8444a0e754
Soloman Garcia <soloman.garcia@gmail.com>
2020-06-16 08:12:34 -0600

    create fetchData function

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log('Hi there!');
+// Fetch movie data using Axios
+const fetchData = async () => {
+  const response = await axios.get('http://www.omdbapi.com/', {
+    params: {
+      apikey: '10d8c1c8',
+      s: 'avengers'
+    }
+  });
+
+  console.log(response.data);
+}
+
+fetchData();

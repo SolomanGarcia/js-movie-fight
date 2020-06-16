abdae9400470ad3066680bef76fd3cd8df1e7a2d
Soloman Garcia <soloman.garcia@gmail.com>
2020-06-16 08:36:50 -0600

    create input for searchTerm

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 // Fetch movie data using Axios
-const fetchData = async () => {
+const fetchData = async (searchTerm) => {
   const response = await axios.get('http://www.omdbapi.com/', {
     params: {
       apikey: '10d8c1c8',
-      i: 'tt0848228'
+      s: searchTerm
     }
   });
 
   console.log(response.data);
-}
+};
 
-fetchData();
+const input = document.querySelector('input');
+input.addEventListener('input', (event) => {
+  fetchData(event.target.value);
+});

81d16fc2c98aba4b3c11374282b2b89b1cca9dad
Soloman Garcia <soloman.garcia@gmail.com>
2020-06-16 08:16:31 -0600

    adjust fetchData function

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const fetchData = async () => {
   const response = await axios.get('http://www.omdbapi.com/', {
     params: {
       apikey: '10d8c1c8',
-      s: 'avengers'
+      i: 'tt0848228'
     }
   });
 
